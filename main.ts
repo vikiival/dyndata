928b28cb4ff3915c67057d761f35c396d502f29d
Viki Val <viktorko99@gmail.com>
2024-02-24 19:47:03 +0100

    :zap: working poc!

--- main.ts
+++ main.ts
@@ -1,10 +1,9 @@
-import { crypto } from "crypto/mod.ts";
-import { cors } from "hono/middleware.ts";
-import { type Context, Hono } from "hono/mod.ts";
-import { Base } from "@/types.ts";
-import { validateRequest as validateBase } from "@/middleware/validateBaseRequest.ts";
-import { CAPTURE_URL, PATH, PORT } from "@/constants.ts";
-// import { IPFS_GW } from "@/constants.ts";
+import { CAPTURE_URL, PATH, PORT } from "@/constants.ts"
+import { validateRequest as validateBase } from "@/middleware/validateBaseRequest.ts"
+import { Base } from "@/types.ts"
+import { hexOf } from '@/utils.ts'
+import { cors } from "hono/middleware.ts"
+import { Hono, type Context } from "hono/mod.ts"
 
 const app = new Hono();
 
@@ -12,7 +11,7 @@ const db = await Deno.openKv(PATH);
 
 app.use("*", cors());
 
-app.get("/", (c: Context) => c.text("Hello world!"));
+app.get("/", (c: Context) => c.redirect('https://github.com/vikiival/dyndata'));
 
 app.put("/:chain/base/:id", validateBase, async (c: Context) => {
   const id = c.req.param("id");
@@ -38,16 +37,14 @@ app.get(
     if (!base) {
       return c.text(`[API] BASE on Chain ${chain} with ID: ${id} found`, 404);
     }
-
-    const encoded = new TextEncoder().encode(sn);
-    const hash = await crypto.subtle.digest("KECCAK-256", encoded);
-
-    // const image = await fetch(`/image/${id}/${sn}`); //?
+  
+    const hash = await hexOf(sn)
+    const url = new URL(c.req.url)
 
     const data: Base = {
       name: `${base.name} #${sn}`,
       description: base.description,
-      image: "",
+      image: `${url.origin}/${chain}/image/${id}/${sn}/?hash=${hash}`,
       animation_url: `${base.animation_url}/?hash=${hash}`,
       external_url: "https://kodadot.xyz/",
     };
@@ -64,8 +61,7 @@ app.get("/:chain/image/:id/:sn", async (c: Context) => {
   // if (!base) {
   //   return c.text(`[API] BASE on Chain ${chain} with ID: ${id} found`, 404);
   // }
-  const encoded = new TextEncoder().encode(sn);
-  const hash = await crypto.subtle.digest("KECCAK-256", encoded);
+  const hash = c.req.query('hash') || await hexOf(sn)
   const url = // base.animation_url.replace("ipfs://", IPFS_GW) + `?hash=${hash}`;
     `https://nftstorage.link/ipfs/bafybeibc4bhdksstboetdg7q7dzzbxl6ynqmhjdifukm3hxirqvebxefea/?hash=${hash}`;
   //  call fetch at capturegl.vercel.app/api/screenshot, POST body: {url: "https://kodadot.xyz/"}
@@ -86,8 +82,12 @@ app.get("/:chain/image/:id/:sn", async (c: Context) => {
   // https://hono.dev/guides/examples#proxy
   const newResponse = new Response(resp.body, resp)
   return newResponse
-  // const blob = await resp.arrayBuffer();
-  // return c.body(blob, { headers: resp.headers });
+  // const arrayBuffer = await resp.arrayBuffer();
+  // const data = encodeBase64(arrayBuffer)
+  // // blob to base64
+  // const x = `data:image\/png;base64,${arrayBuffer}`;
+  // // return c.body(x, { headers: resp.headers });
+  // return c.text(data);
 });
 
 Deno.serve({ port: PORT }, app.fetch);
