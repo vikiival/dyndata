143c122185888f95acb8d1fe11860d376afa5b00
Viki Val <viktorko99@gmail.com>
2024-02-24 19:44:48 +0100

    Update validation rules for animation_url

--- middleware/validateBaseRequest.ts
+++ middleware/validateBaseRequest.ts
@@ -1,12 +1,13 @@
 import { type Context } from "hono/mod.ts"
-import { minLength, object, parse, startsWith, string } from "valibot/mod.ts"
+import { minLength, object, parse, startsWith, string, regex } from "valibot/mod.ts"
 
 const baseSchema = object({
   name: string([minLength(3, "Valid name is required")]),
   description: string([minLength(3, "Valid description is required")]),
-  animationUrl: string([
+  animation_url: string([
     startsWith("ipfs://", "possibly not ipfs"),
     minLength(30, "Valid hash is required"),
+    regex(/[^/]$/, "Cannot have trailing slash at the end"),
   ]),
 });
 
