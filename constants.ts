f78e67e8da9ffc8fde680a7ef0aab5e309cd91bd
Viki Val <viktorko99@gmail.com>
2024-02-24 18:15:43 +0100

    :wrench: :tada: constants

---
+++ constants.ts
@@ -0,0 +1,6 @@
+export const CAPTURE_URL = 'https://capture.kodadot.workers.dev/screenshot'
+export const CAPTURE_VERCEL_URL = 'https://capturegl.vercel.app/api/screenshot'
+export const IPFS_GW = 'https://nftstorage.link/ipfs/'
+
+export const PORT = 8080; // 80 for production
+export const PATH = "./db.sqlite";
